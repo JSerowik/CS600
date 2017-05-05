2205d167abf6de943246e9072af44a01dae788c3
jserowik <jserowik@protonmail.com>
2017-05-05 17:17:08 -0400

    Added NLTK library

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-
+import nltk
+nltk.download()
 # Opens Text file with text from a scraped webpage
 logr_file = open("Data/cnbcE_adjusted-for-inflation-the-federal-minimum-wage-is-worth-less-than-50-years-ago.html.txt")
 data = logr_file.read()
@@ -10,11 +11,15 @@ for char in charDel:
 numDel = list(range(10))
 for num in numDel:
 	data = data.replace(str(num),"")
+stopDel = list(stopwords.words('english'))
+for word in stopDel:
+	data = data.replace(str(word),"")
 # Splits data into a list
 datAr = data.split()
 wordList = []
 for word in datAr:
 	if word not in wordList:
 		wordList.append(word)
+# Creates list of articles, prepositions, and pronouns to remove
 		 
 print wordList
