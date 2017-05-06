a7d34c0f422d7ca9d0ac9ebf1efe522d127cc6f2
JSerowik <JSerowik@protonmail.com>
2017-05-05 21:56:23 -0400

    Adjusted the data file names

--- README.md
+++ README.md
@@ -1,38 +1,27 @@
-# CS600
 CS 600 project Web Search Project
 Requirements:
 Implement the simplified Search Engine described in Section 23.5.4 for the pages of a small Web site. Use all the words in the pages of the site as index terms, excluding stop words such as articles, prepositions, and pronouns.
 
 Submit the following four files:
-
 1. A read me file that contains details of your approach to the problem.
-
 2. Your coded, well-commented code file in your favorite language, such as Python, Java, C++,... 
-
 3. The input file that contains the few pages you have used as input, including some links to your other pages..
-
 4. Output file that has samples of your run. Make sure you have tested the boundary conditions.
 
 Q and A:
-
 1. Should I use internet documents or create my own?
-
 Answer: You may download several pages (5 to 10) from Internet, or develop them yourself, and use them as input. Make sure there are some hyperlink between the documents and you can add more pages to your existing ones that is gathered by Web Crawler.
 
 2. Can I use Other Search Engine or Data Structures?
-
 Answer: No. You shall implement what is described in Section 23.5.4. using the approach specified. You are not allowed to use Algorithms and Data Structures that are not covered in the textbook, but you can develop new algorithms based on the data structures that you have seen and proved in the textbook.
 
 3. What criteria should I use for ranking?
-
 Answer: I leave that to you to come up with some simple criteria and algorithm to implement ranking, such as the number of times a word has appeared in the document or any other idea you have. But please explain your approach.
 
 4. Can I provide screenshots of my output?
-
 Answer: Yes. 
 
 5. How can I read the web pages in?
-
 Answer:
 If you are using java, use jsoup: (https://jsoup.org/ (Links to an external site.)Links to an external site.) .
 
--- project.py
+++ project.py
@@ -1,25 +1,100 @@
-import nltk
-nltk.download()
-# Opens Text file with text from a scraped webpage
-logr_file = open("Data/cnbcE_adjusted-for-inflation-the-federal-minimum-wage-is-worth-less-than-50-years-ago.html.txt")
-data = logr_file.read()
-logr_file.close()
-# Create list of characters to delete
-charDel = [".",",","/","?","<",">",":",";","[","]","{","}","-","_","+","=","|",'"',"!","@","#","$","%","^","&","*","(",")"]
-for char in charDel:
-	data = data.replace(char,"")
-numDel = list(range(10))
-for num in numDel:
-	data = data.replace(str(num),"")
-stopDel = list(stopwords.words('english'))
-for word in stopDel:
-	data = data.replace(str(word),"")
-# Splits data into a list
-datAr = data.split()
-wordList = []
-for word in datAr:
-	if word not in wordList:
-		wordList.append(word)
-# Creates list of articles, prepositions, and pronouns to remove
-		 
-print wordList
+#By Justin Serowik
+from nltk.corpus import stopwords
+from collections import Counter
+
+# Function that outputs occurrence lists
+def textCount(data):
+	# Create list of characters to delete
+	charDel = [".",",","/","?","<",">",":",";","[","]","{","}","-","_","+","=","|",'"',"!","@","#","$","%","^","&","*","(",")"]
+	for char in charDel:
+		data = data.replace(char,"")
+	numDel = list(range(10))
+	for num in numDel:
+		data = data.replace(str(num),"")
+	# Splits data into a list
+	datAr = data.split()
+	# Creates a list of stopwords to delete using the NLTK library
+	stopDel = list(str(stopwords.words('english')))
+	# Adding extra words not covered above
+	ex = ["the", "The", "to","and", "of", "for", "is"]
+	for w in ex:
+		stopDel.append(w)
+	for word in datAr:
+		if word in stopDel:
+			datAr.remove(word)
+	# Uses Counter to make an occurrence list which is stored in a dictionary
+	counts = Counter(datAr)
+	index_terms = counts.items()
+	return index_terms
+# Function to create a trie using a list of words
+end = 'last'
+def create_trie(words):
+	root = {}
+	ind = 0
+	for word in words:
+		current_dict = root
+		for letter in word:
+			current_dict = current_dict.setdefault(letter, {})
+		current_dict[end] = ind
+		ind = ind + 1
+	return root
+# Function to find and return the index value from a trie
+def test_trie(trie, word):
+	current_dict = trie
+	for letter in word:
+		if letter in current_dict:
+			current_dict = current_dict[letter]
+		else:
+			return 0
+	else:
+		if end in current_dict:
+			return current_dict[end]
+		else:
+			return 0
+
+# Function that gets tries from all 8 sources
+trie = []
+indexTerms = []
+def trieForm():
+	for i in range(0,8):
+		# Opens Text file with text from a scraped webpage
+		file = "Data/"+(str(i))+".txt"
+		logr_file = open(file)
+		dt = logr_file.read()
+		logr_file.close()
+		# returns text count
+		iterms = textCount(dt)
+		indexTerms.append(textCount(dt))
+		# creates list of words from the dictionary to make a trie out of
+		wordList=[]
+		for w in iterms:
+			wordList.append(w[0])
+		trie.append(create_trie(wordList))
+#Creates tries and occurance lists for each webpage
+trieForm()
+# creates the rank list for searching
+rank = []
+def search(term):
+	# create temp list to split search term into
+	temp = []
+	temp = term.split()
+	print "Search terms:"
+	print temp
+	# iterate through every trie
+	for i in range(0,8):
+		sum = 0
+		#iterate through every search term
+		for t in temp:
+			# If search finds the term, it adds the occurrences to the rank
+			if test_trie(trie[i], t) > 0:
+				# Uses index at the bottom of trie to find number of occurrences in the index terms
+				sum = sum + int(indexTerms[i][test_trie(trie[i], t)][1])
+		rank.append(sum)
+	return rank
+
+#Test the search terms to return the ranks from the search
+test = search("Atlanta Obama unemployment")
+# Iterate through the ranks and output
+for it in range(0,7):
+	print "Webpage "+str(it)+" rank:"
+	print test[it]
