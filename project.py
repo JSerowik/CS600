68795edffb93fa925bc0d58ffd7ce84fc84e127b
jserowik <jserowik@protonmail.com>
2017-08-15 16:14:18 -0400

    Added menu to project to help refresh news sources and select what kind of output is desired.

--- project.py
+++ project.py
@@ -1,49 +1,50 @@
-#By Justin Serowik
+# By Justin Serowik
+import datetime
+import requests
+
 from nltk.corpus import stopwords
 from collections import Counter
+from boilerpipe.extract import Extractor
 from bs4 import BeautifulSoup
-from datetime import datetime
-#from boilerpipe.extract import Extractor
-import mechanize
+from goose import Goose
 
 # Function to scrape RSS feed for text data, used to get text documents
+
 def scrape(feed, used, excep, split1, split2, urlName, nameF):
-	# Opens RSS feed
-	response = mechanize.urlopen(feed)
-	html_str = response.read()
-	soupRss = BeautifulSoup(html_str, 'html.parser')
-	# Sets link lists
 	arrLinks = []
-	usedLinks = []
-	logr_file = open(used,"r")
-	usedLinks = [line.strip() for line in logr_file]
-	logr_file.close()
-	# Extracts links from inital feed, excluding non-news
-	for link in soupRss.find_all('link'):
-		if link.get_text() != excep:
-			arrLinks.append(str(link.get_text()))
-	# Checks list of already queried links
+	req = requests.get('http://feeds.reuters.com/reuters/businessNews')
+	soupRss = BeautifulSoup(req.text, "html5lib")
+
+	logrFile = open(used,"r")
+	usedLinks = [line.strip() for line in logrFile]
+	logrFile.close()
+
+	for link in soupRss.find_all('guid'):
+		arrLinks.append(str(link.getText().replace('?feedType=RSS&feedName=businessNews', '')))
+
 	log_file = open(used,"w")
 	for item in arrLinks:
 		log_file.write(str(item)+"\n")
 	log_file.close()
-	# Finds and extracts stripped news content with timestamp, omitting used links
+
 	for i in range(0, 8):
 		fileName = arrLinks[i].rsplit('/', split1)[split2]
-		if any(fileName in s for s in usedLinks):
-			print fileName +" has been extracted."
-		else:
-			extractedText = Extractor(extractor='ArticleExtractor', url=urlName+fileName)
-			print fileName
-			write_file = open("extractedFiles/"+str(i)+".txt","w")
-			write_file.write(str(datetime.now())+"\n")
-			write_file.write(str(extractedText.getText().encode("utf-8")))
-			write_file.close()
+		#if any(fileName in s for s in usedLinks):
+		#	print fileName +" has been extracted."
+		#else:
+		extractedText = Extractor(extractor='ArticleExtractor', url=urlName+fileName)
+		print fileName
+		write_file = open("Data/"+str(i)+".txt","w")
+		write_file.write(str(datetime.date.today()) + "\n")
+		write_file.write(str(extractedText.getText().encode("utf-8")))
+		write_file.close()
+
 
 # Function that outputs occurrence lists
 def textCount(data):
 	# Create list of characters to delete
-	charDel = [".",",","/","?","<",">",":",";","[","]","{","}","-","_","+","=","|",'"',"!","@","#","$","%","^","&","*","(",")"]
+	charDel = [".",",","/","?","<",">",":",";","[","]","{","}","-",
+	"_","+","=","|",'"',"!","@","#","$","%","^","&","*","(",")"]
 	for char in charDel:
 		data = data.replace(char,"")
 	numDel = list(range(10))
@@ -96,7 +97,7 @@ indexTerms = []
 def trieForm():
 	for i in range(0,8):
 		# Opens Text file with text from a scraped webpage
-		file = "Data/"+(str(i))+".txt"
+		file = "Data/"+(str(i)) + ".txt"
 		logr_file = open(file)
 		dt = logr_file.read()
 		logr_file.close()
@@ -112,12 +113,17 @@ def trieForm():
 trieForm()
 # creates the rank list for searching
 rank = []
-def search(term):
+def search(term, fileOut):
 	# create temp list to split search term into
 	temp = []
 	temp = term.split()
-	print >> f,"Search terms:"
-	print >> f, temp
+	if fileOut:
+		print >> f,"Search terms:"
+		print >> f, temp
+	else:
+		print("Search terms:")
+		print(temp)
+
 	# iterate through every trie
 	for i in range(0,8):
 		sum = 0
@@ -130,29 +136,67 @@ def search(term):
 		rank.append(sum)
 	return rank
 
-
-f = open('test3.txt', 'w')
-#Test the search terms to return the ranks from the search
-test = search("Boston cities dollar")
-# Iterate through the ranks and output
-best = [None, 0]
-for it in range(0,8):
-	print >> f, "Webpage "+str(it)+" rank:"
-	print >> f, test[it]
-	if best[1] < test[it]:
-		best[0] = it
-if best[0] == None:
-	print >> f, "Search result not found in any of the webpages."
-else:
-	print >> f, "Highest ranking search from Webpage " + str(best[0])
-print >> f, "\n"
-print >> f, "Index Terms Output"
-for j in range(0, 8):
-	print >> f, "Webpage "+str(j)
-	print >> f, indexTerms[j]
-print >> f, "\n"
-print >> f, "Tries Output"
-for k in range(0, 7):
-	print >> f, "Webpage "+str(k)
-	print >> f, trie[k]
-f.close()
+menu = True
+while menu:
+	print ("""
+	1. Extract News
+	2. Read to Text File
+	3. Read to Console
+	4. Exit/Quit
+	""")
+	menu = raw_input("Enter input: ") 
+	if menu == "1": 
+		scrape("http://feeds.reuters.com/reuters/businessNews","usedReuter.txt",'http://www.reuters.com',1,-1,"http://www.reuters.com/article/","reuters_")
+	elif menu == "2":
+		f = open('test3.txt', 'w')
+		#Test the search terms to return the ranks from the search
+		test = search("Boston cities dollar", True)
+		# Iterate through the ranks and output
+		best = [None, 0]
+		for it in range(0,8):
+			print >> f, "Webpage " + str(it) + " rank:"
+			print >> f, test[it]
+			if best[1] < test[it]:
+				best[0] = it
+		if best[0] == None:
+			print >> f, "Search result not found in any of the webpages."
+		else:
+			print >> f, "Highest ranking search from Webpage " + str(best[0])
+		print >> f, "\n"
+		print >> f, "Index Terms Output"
+		for j in range(0, 8):
+			print >> f, "Webpage " + str(j)
+			print >> f, indexTerms[j]
+		print >> f, "\n"
+		print >> f, "Tries Output"
+		for k in range(0, 7):
+			print >> f, "Webpage " + str(k)
+			print >> f, trie[k]
+		f.close()
+	elif menu == "3":
+		test = search("Boston cities dollar", False)
+		# Iterate through the ranks and output
+		best = [None, 0]
+		for it in range(0,8):
+			print("Webpage " + str(it) + " rank:")
+			print(test[it])
+			if best[1] < test[it]:
+				best[0] = it
+		if best[0] == None:
+			print("Search result not found in any of the webpages.")
+		else:
+			print("Highest ranking search from Webpage " + str(best[0]))
+		print("\n")
+		print("Index Terms Output")
+		for j in range(0, 8):
+			print("Webpage " + str(j))
+			print(indexTerms[j])
+		print("\n Tries Output")
+		for k in range(0, 7):
+			print("Webpage " + str(k))
+			print(trie[k])
+	elif menu == "4":
+		print("Goodbye")
+		menu = False 
+	elif menu != "":
+		print("\n Not Valid Choice Try again") 
